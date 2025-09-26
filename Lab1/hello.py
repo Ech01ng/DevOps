7d66b05efac9cd1d191586cb83fb4a29e9a2bb0e
erik <erikjhl14@gmail.com>
2025-09-26 10:59:01 +0100

    first commit

---
+++ Lab1/hello.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '''
+    <p>Hello world, I am a FLASK APP!</p>
+    <p>Go to the <a href="/about">About Page</a>.</p>
+    '''
+    
+@app.route('/about')
+def about():
+    return '''
+    <p>This is the About Page.</p>
+    <p>Go back to the <a href="/">Home Page</a>.</p>
+    '''
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
